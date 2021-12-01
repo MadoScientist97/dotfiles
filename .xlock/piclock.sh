5a1f23062983539a7a37cb625a65f1b73234a45b
MadoScientist97 <sahilthehi3r7@gmail.com>
2021-12-01 11:42:47 +0530

    Corrected Lock Icon Path.

--- .xlock/piclock.sh
+++ .xlock/piclock.sh
@@ -4,7 +4,7 @@ exec >>"$HOME/.xlock/run.log" 2>&1
 set -x
 printf 'Starting\n'
 WorkingD=$HOME/.xlock
-icon=$WorkingD/lock.png
+icon=$WorkingD/pics/lock.png
 pics=$(find $WorkingD/pics/ -type f | shuf -n 1)
 color="$(openssl rand -hex 3)"
 convert $pics $icon -gravity center -composite -matte $WorkingD/.temp.png
