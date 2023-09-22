17db6c0ae0c1f94ce6bfbbdee48fa1f5f29f8c42
MadoScientist97 <sahilthehi3r7@gmail.com>
2023-09-22 07:39:05 +0530

    Refactored for multi monitor support with different res

--- .xlock/piclock.sh
+++ .xlock/piclock.sh
@@ -7,6 +7,19 @@ WorkingD=$HOME/.xlock
 icon=$WorkingD/pics/lock.png
 pics=$(find $WorkingD/pics/ -type f | shuf -n 1)
 color="$(openssl rand -hex 3)"
-convert $pics $icon -gravity center -composite -matte $WorkingD/.temp.png
+res=$(xrandr | grep -F '*' | awk '{print $1}')
+convert $pics -resize $res $icon -gravity  center -composite -matte $WorkingD/.temp.png &
+wait
 i3lock -c $color -i $WorkingD/.temp.png
-rm $WorkingD/.temp.png
+/usr/bin/rm $WorkingD/.temp.png
+
+# log cleanup
+WorkingD=$HOME/.xlock
+numlines=$(wc -l "$WorkingD/run.log" | awk '{print $1}')
+echo $numlines
+if (( $numlines > 10000 )) 
+then                                                                                      
+  mv $WorkingD/run.log $WorkingD/run.log.big                                                                            
+  tail -10000 $WorkingD/run.log.big > $WorkingD/run.log                                                                 
+  /usr/bin/rm $WorkingD/run.log.big                                                                 
+fi
